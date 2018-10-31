0687061eecc9f7970b1f96f4ce3a29b95e5b982c
Darren Lim <darrenlim193@gmail.com>
2018-10-31 21:59:51 +1100

    - Added a landing page for logging in - Made devise redirect to this landing page AFTER user has logged in - Set routes to show the landing page - Devise redirect is configured in application_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,8 @@ class PagesController < ApplicationController
     @website_description = ":Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Phasellus rhoncus. Integer in sapien. Etiam commodo dui eget wisi. Ut tempus purus at lorem. Nullam eget nisl. Mauris dictum facilisis augue. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent id justo in neque elementum ultrices. In convallis. "
 
   end
+
+  def landing_page_login
+    @user = current_user
+  end
 end
