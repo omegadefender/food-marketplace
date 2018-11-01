6a171e074cf5b574635e64144a07a85510170d3d
Darren Lim <darrenlim193@gmail.com>
2018-11-01 14:15:14 +1100

    see previous commit

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-if ['development', 'test'].include? ENV['RAILS_ENV','PUBLISHABLE_KEY','SECRET_KEY']
+if ['development', 'test'].include? ENV['RAILS_ENV']
   Dotenv::Railtie.load
 end
 
