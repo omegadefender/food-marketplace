168da5e69fe8b1170f11f4a5f7108012baeb626a
Darren Lim <darrenlim193@gmail.com>
2018-11-01 14:17:08 +1100

    Trying to break fix

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-if ['development', 'test'].include? ENV['RAILS_ENV']
-  Dotenv::Railtie.load
-end
+Dotenv::Railtie.load
+
 
 
 module FoodMarketplace
