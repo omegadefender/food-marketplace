8ec9bd5242f825cce6860e86ea3ff99f176e8a9d
Ben Gelbard <bengelbard@Bens-MacBook-Pro-3.local>
2018-11-02 10:55:15 +1100

    added searchbar

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   root to: 'pages#welcome'
+
+  post 'products/search' => 'products#search', as: 'search_products'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
